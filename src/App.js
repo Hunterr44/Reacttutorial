3ed560668189a872341c34341cdcda1b3d73d8d7
Hunterr44 <91934777+Hunterr44@users.noreply.github.com>
2022-07-06 18:55:14 -0500

    Big Progress

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import './App.css';
 
 const schedule = {
   title: "CS Courses for 2022-23" ,
@@ -31,7 +32,7 @@ const Banner = ({title}) => (
 );
 
 const CourseList = ({courses}) => (
-  <div>
+  <div className = "container">
     { Object.values(courses).map(course => <Course key = {courses.id} Course course = { course } /> ) }
     </div>
 );
@@ -47,16 +48,38 @@ const getCourseNumber = course => (
 );
 
 const Course = ({ course }) => (
-  <div>
-    {getCourseTerm(course)} CS { getCourseNumber(course) }: {course.title}
+  <div className="card m-1 p-2">
+    <div className="card-body">
+      <div className = "card-title">{getCourseTerm(course)} CS { getCourseNumber(course)}</div>
+      <div className = "card-text"> {course.title}</div> 
     </div>
-)
+  </div>
+);
 
-const App = () => (
-  <div>
+
+
+const App = () => { 
+  const [schedule, setSchedule] = useState();
+  const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php';
+
+  useEffect(() => {
+    const fetchSchedule = async () => {
+      const response = await fetch(url);
+      if(!response.ok) throw response;
+      const json = await response.json();
+      setSchedule(json);
+  }
+  fetchSchedule();
+  }, [])
+
+  if (!schedule) return <h1>Loading Schedule...</h1>;
+
+  return (
+    <div className = "container">
     <Banner title = { schedule.title}/>
     <CourseList courses = {schedule.courses} />
   </div>
 );
+};
 
 export default App;
