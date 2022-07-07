623bb42ecc0af4d1e5a0d229dab8d73ce13294a3
Hunterr44 <91934777+Hunterr44@users.noreply.github.com>
2022-07-07 13:50:15 -0500

    Filtah Bruv

--- src/App.js
+++ src/App.js
@@ -31,11 +31,18 @@ const Banner = ({title}) => (
   <h1>{title}</h1>
 );
 
-const CourseList = ({courses}) => (
-  <div className = "container">
-    { Object.values(courses).map(course => <Course key = {courses.id} Course course = { course } /> ) }
+const CourseList = ({courses}) => {
+  const [term, setTerm] = useState('Winter');
+  const termCourses = Object.values(courses).filter(course => term === getCourseTerm(course));
+  return (
+  <>
+  <TermSelector term = {term} setTerm = {setTerm} />
+  <div className = "course-list">
+    { termCourses.map(course => <Course key = {courses.id} Course course = { course } /> ) }
     </div>
+    </>
 );
+};
 
 const terms = {F: 'Fall', W: 'Winter', S: 'Spring'};
 
@@ -54,6 +61,24 @@ const Course = ({ course }) => (
       <div className = "card-text"> {course.title}</div> 
     </div>
   </div>
+);
+
+const TermSelector = ({term, setTerm}) => (
+  <div className = "btn-group">
+    {
+      Object.values(terms).map(value => <TermButton key = {value} term = {value} setTerm = {setTerm} checked = {value === term} />)
+    }
+    </div>
+);
+
+const TermButton = ({term, setTerm, checked}) => (
+  <>
+    <input type="radio" id={term} className="btn-check" autoComplete="off" checked={checked}
+    onChange = {() => setTerm(term)} />
+    <label class="btn btn-success m-1 p-2" htmlFor={term}>
+    { term }
+    </label>
+  </>
 );
 
 
